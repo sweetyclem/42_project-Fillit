49e9798584b49ca88107e6ed20cf64ef9213f46a
Clementine PIRLOT <cpirlot@e1r12p14.42.fr>
2017-11-20 13:56:28 +0100

    add function to assign letters

---
+++ assign_letters.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   assign_letters.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/20 12:59:00 by cpirlot           #+#    #+#             */
+/*   Updated: 2017/11/20 13:32:34 by cpirlot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+t_tetri	*assign_letter(t_tetri *tetri_list)
+{
+	char	c;
+	t_tetri	*tmp;
+
+	c = 'A';
+	tmp = tetri_list;
+
+	while (tmp || c <= 'Z')
+	{
+		tmp->letter = c;
+		ft_putchar(tmp->letter);
+		c++;
+		tmp = tmp->next;
+	}
+	return (tetri_list);
+}
