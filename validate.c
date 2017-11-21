9dd7d71e10c7087cc7791e41fae921e2b4c911be
Clementine PIRLOT <cpirlot@e1r11p8.42.fr>
2017-11-21 07:05:09 +0100

    add error handling

--- validate.c
+++ validate.c
@@ -6,7 +6,7 @@
 /*   By: cpirlot <cpirlot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 09:36:56 by cpirlot           #+#    #+#             */
-/*   Updated: 2017/11/16 09:51:42 by cpirlot          ###   ########.fr       */
+/*   Updated: 2017/11/21 07:02:20 by cpirlot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,36 @@ static int	is_pound(char *block, int pos)
 	return (0);
 }
 
+int			check_char(char c, int i)
+{
+	if (c != '.' && c != '#' &&
+		c != '\n' && c != '\0')
+		return (0);
+	if (i % 5 == 4 && c != '\n')
+		return (0);
+	return (1);
+}
+
 int			block_valid(char *block)
 {
 	int	nb_pounds;
 	int	nb_lines;
 	int	i;
 
-	i = 0;
+	i = 1;
 	nb_pounds = 0;
 	nb_lines = 0;
 	if (block[0] != '.' && block[0] != '#')
 		return (0);
-	while (block[i] != '\0')
+	while (block[i++] != '\0')
 	{
-		if (block[i] != '.' && block[i] != '#' &&
-			block[i] != '\n' && block[i] != '\0')
+		if (!check_char(block[i], i))
 			return (0);
-		if (i % 5 == 4 && block[i] == '\n')
+		if (i % 5 == 0 && block[i] == '\n')
 			nb_lines++;
-		if (i % 5 == 4 && block[i] != '\n')
+		else
+			return (0);
+		if (i % 5 != 0 && (block[i] != '.' || block[i] != '#'))
 			return (0);
 		if (is_pound(block, i))
 		{
@@ -46,11 +57,8 @@ int			block_valid(char *block)
 				is_pound(block, i - 1) == 0 && is_pound(block, i - 5) == 0)
 				return (0);
 		}
-		if (nb_pounds > 4)
-			return (0);
-		i++;
 	}
-	if (nb_lines != 4)
+	if (nb_pounds > 4)
 		return (0);
-	return (1);
+	return (nb_lines != 4 ? 0 : 1);
 }
